d6890129469da0f388f17084609086387e04577b
d-cdmon <dani@alturron.top>
2025-05-19 08:07:40 +0200

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,6 @@
+// .eleventy.js
+
+module.exports = function(eleventyConfig) {
+  // Configuración opcional para procesar archivos Markdown
+  eleventyConfig.addPassthroughCopy("css"); // Para copiar los archivos CSS sin procesar
+};
